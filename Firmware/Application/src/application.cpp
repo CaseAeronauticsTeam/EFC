dca479c08731376b3d217627318bc45708fdb13a
mitchelw <57276362+mitchelw@users.noreply.github.com>
2021-10-27 19:47:22 -0400

    Intermediate changes for SPI

--- Firmware/Application/src/application.cpp
+++ Firmware/Application/src/application.cpp
@@ -34,14 +34,23 @@ void init()
 	leftAileron.init(TIM1, &htim1, 1, 180);
 	rightAileron.init(TIM1, &htim1, 2, 130);
 
-	uint8_t tx_buf = 0;
-	uint16_t rx_buf = 0;
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_SET);
 
+	for (int i = 0; i < 2; i++)
+	{
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+		HAL_Delay(1000);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+		HAL_Delay(1000);
+	}
 
-	 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_RESET);
-	 HAL_SPI_Transmit(&hspi1, (uint8_t *)&tx_buf, 1, 100);
-	 HAL_SPI_Receive(&hspi1, (uint8_t *)&rx_buf, 2, 100);
-	 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_SET);
+//	uint8_t tx_buf = 0b001;
+//	uint16_t rx_buf = 0;
+//
+//	 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_RESET);
+//	 HAL_SPI_Transmit(&hspi1, (uint8_t *)&tx_buf, 1, 100);
+//	 HAL_SPI_Receive(&hspi1, (uint8_t *)&rx_buf, 2, 100);
+//	 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_SET);
 }
 
 void launch()
@@ -58,8 +67,30 @@ void launch()
 		HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
 		raw = HAL_ADC_GetValue(&hadc1);
 
-		if (!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13))
+
+
+		uint8_t tx_buf = 0b01100001;
+		uint16_t rx_buf = 0;
+
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_RESET);
+		HAL_SPI_Transmit(&hspi1, (uint8_t *)&tx_buf, 1, 100);
+		HAL_SPI_Receive(&hspi1, (uint8_t *)&rx_buf, 1, 100);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_SET);
+
+
+
+
+//		if (!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13))
+		if (rx_buf == 0b10100000 || HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13))
 		{
+			leftAileron.setAngle(0);
+			rightAileron.setAngle(0);
+			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+
+		}
+		else if (rx_buf != 0b10100000 || !HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13))
+		{
+			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
 			// Assign Left Aileron Servo to ADC
 			thetaL = (((double)raw - (double)((1 << 11) - 1)) / (double)((1 << 11) - 1)) * 90.;
 			leftAileron.setAngle(thetaL);
@@ -67,11 +98,6 @@ void launch()
 			// Cycle Right Aileron Servo slowly
 			thetaR = thetaR < 90 ? (thetaR + .0002) : -90;
 			rightAileron.setAngle(thetaR);
-		}
-		else  // "Kill"
-		{
-			leftAileron.setAngle(0);
-			rightAileron.setAngle(0);
 		}
 
 	}
